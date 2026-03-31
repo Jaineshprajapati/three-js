8d4431152c91c09a3edeaf6f3f9093323dfd09df
JaineshOrionik <106659264+Jaineshprajapati@users.noreply.github.com>
2026-03-31 23:31:56 +0530

    practice

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-// Basic Three.js scene setup
+import * as THREE from "three";
+import vertex from "./shaders/vertex.glsl?raw";
+import fragment from "./shaders/fragment.glsl?raw";
 
-import * as THREE from 'three';
 // Create scene
 const scene = new THREE.Scene();
 
 // Create camera
 const camera = new THREE.PerspectiveCamera(
-  75, 
-  window.innerWidth / window.innerHeight, 
-  0.1, 
-  1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
 );
 
 // Create renderer
-const canvas = document.querySelector('#canvas');
-const renderer = new THREE.WebGLRenderer({canvas});
+const canvas = document.querySelector("#canvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 // Add a cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x0077ff });
+const material = new THREE.ShaderMaterial({
+  vertexShader: vertex,
+  fragmentShader: fragment,
+});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -40,7 +44,7 @@ function animate() {
 animate();
 
 // Resize renderer with window
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
---
+++ shaders/fragment.glsl
@@ -0,0 +1,5 @@
+// Basic Three.js fragment shader
+
+void main() {
+    gl_FragColor = vec4(1.0, 2, 0.2, 1.0); // Orange-ish color
+}

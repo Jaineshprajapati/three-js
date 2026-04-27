95fc15e3e3a829980963b3a0103e6eaea2d51b8b
JaineshOrionik <106659264+Jaineshprajapati@users.noreply.github.com>
2026-04-27 15:20:45 +0530

    cloth done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import vertex from "./shaders/vertex.glsl?raw";
 import fragment from "./shaders/fragment.glsl?raw";
 
@@ -19,8 +20,12 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+// Add Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
 // Add a cube
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.PlaneGeometry(2, 2, 200, 200);
 const material = new THREE.ShaderMaterial({
   vertexShader: vertex,
   fragmentShader: fragment,
@@ -38,7 +43,8 @@ camera.position.z = 3;
 function animate() {
   requestAnimationFrame(animate);
 
-  material.uniforms.uTime.value += 0.1;
+  material.uniforms.uTime.value += 0.05;
+  controls.update();
   renderer.render(scene, camera);
 }
 animate();
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,14 +1,11 @@
 varying vec2 vUv;
 
 uniform float uTime;
+varying float noise;
 
 void main() {
-    
-    // float green = smoothstep(0.2, 0.8, vUv.y) * 0.39;
-    // vec4 color = vec4(0.0, green, 0.0, 1.0);
-
-   float val = clamp(-1., 1., (vUv.y*2.)-1.);
-   vec4 color = vec4(val, val, val, 1.0);
-
-    gl_FragColor = color;
+    // Solid red color, with noise added to brightness
+    float red = 1.0 + noise;
+    red = clamp(red, 0.0, 1.0);
+    gl_FragColor = vec4(red, 0.0, 0.0, 1.0);
 }
